55942ad0de68395bce02862c0c3e1935e531cdbf
Swastik Majhi <majhiswastik21@gmail.com>
2024-09-29 12:13:50 +0530

    fix list loading

--- src/content.ts
+++ src/content.ts
@@ -1,43 +1,44 @@
 import { Message, Response } from "../utils/index";
 import { songInfo } from "../utils/type";
 
-// const getSongList = () => {
+const getSongList = async () => {
 
-//   setTimeout(async () => {
-//     const songs : Array<songInfo> = []
-//     const list = document.querySelectorAll(
-//       ".songs-list-row__song-name-wrapper"
-//     );
+  // setTimeout(async () => {
 
-//     list.forEach((el) => {
-//       const songInfo:songInfo = {
-//         song : el?.querySelector(".songs-list-row__song-name")?.textContent ?? "",
-//         artist : el?.querySelector(".songs-list-row__by-line > span")?.textContent ?? "",
-//       }
-//       songs.push(songInfo)
-//     })
-
-    
-//     try {
-//       const response = await chrome.runtime.sendMessage(new Message("song_list", songs));
-//       if (response.success) console.log("content loaded on popup successfully")
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   },1000)
-// };
+  // },1000)
 
 
-// document.readyState ? getSongList() : document.addEventListener("DOMContentLoaded", getSongList);
+  const songs : songInfo[] = [];
+  const list = document.querySelectorAll(
+    ".songs-list-row__song-name-wrapper"
+  );
 
-console.log("new loaded");
+  list.forEach((el) => {
+    const songInfo:songInfo = {
+      song : el?.querySelector(".songs-list-row__song-name")?.textContent ?? "",
+      artist : el?.querySelector(".songs-list-row__by-line > span")?.textContent ?? "",
+    }
+    songs.push(songInfo)
+  })
+
+  
+  try {
+    const response = await chrome.runtime.sendMessage(new Message("update_list", songs));
+    if (response.success) console.log("content loaded on popup successfully")
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  function(request: Message<null>, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting === "hello")
-      sendResponse(true);
+    if(request.type === "scan_request") {
+      console.log("Scan request received");
+      getSongList();
+      sendResponse(new Response(true, null));
+    }
   }
 );
--- src/popup.ts
+++ src/popup.ts
@@ -1,20 +1,51 @@
-import { Message,Response } from "../utils/index"
+import { Message, Response } from "../utils/index";
 import { songInfo } from "../utils/type";
 
 const list = document.querySelector(".song_list");
 const scanButton = document.querySelector(".scan_button");
 
 scanButton?.addEventListener("click", async () => {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    if(tab && typeof tab.id === "number") {
-        const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-  // do something with response here, not outside the function
-        console.log(response);
-        
-    }
-})
+  console.log("button clicked");
+  const [tab] = await chrome.tabs.query({ active: true });
+  console.log(tab);
 
-// message.forEach(element => {
-//     const listItem = list?.appendChild(document.createElement("li"))
-//    if(listItem) listItem.textContent = `${element.song} by ${element.artist}`        
-// });
+  if (tab && typeof tab.id === "number") {
+    console.log("tab detected");
+    const response = await chrome.tabs.sendMessage(
+      tab.id,
+      new Message("scan_request", null)
+    );
+    console.log(response);
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (
+  request: Message<songInfo[]>,
+  sender,
+  sendResponse
+) {
+  console.log(
+    sender.tab
+      ? "from a content script:" + sender.tab.url
+      : "from the extension"
+  );
+  if (request.type === "update_list") {
+    console.log("update request received");
+
+    if (request.message.length === 0) {
+      console.log("No songs detected");
+      sendResponse(new Response(false, null));
+      process.exit(1);
+    }
+
+    request.message.forEach((element) => {
+      const listItem = list?.appendChild(document.createElement("li"));
+      if (listItem)
+        listItem.textContent = `${element.song} by ${element.artist}`;
+    });
+    
+    console.log("list Updated");
+    
+    sendResponse(new Response(true, null));
+  }
+});
